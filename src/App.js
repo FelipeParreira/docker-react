0be71066209e4bd7e7660c7f375c9a8cae156c2a
Felipe <felipeparreirafb@gmail.com>
2019-06-10 19:54:07 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          I WAS CHANGED AGAIN AGAIN
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
